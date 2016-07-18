bba9aaac10458c280c2dcd4f395bf8583f676157
Robert Quitt <robertquitt@gmail.com>
2016-07-18 04:25:34 +0000

    vision app now tests camera

--- app/vision_app.py
+++ app/vision_app.py
@@ -1,5 +1,25 @@
 import sys
 sys.path.append("../include/")
-import cv2
+import cv2 as cv
 import numpy as np
+import pydsm as dsm
+import time
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+ 
+# initialize the camera and grab a reference to the raw camera capture
+
+# allow the camera to warmup
+time.sleep(1)
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+ 
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+ 
+# display the image on screen and wait for a keypress
+cv.imshow("Image", image)
+cv.waitKey(0)
 
